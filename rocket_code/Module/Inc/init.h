ff5fcdf5d22fdde61ed4b1728459a2b5f796c6fe
Dat Hoang <csdathoang88@gmail.com>
2025-05-05 00:39:14 +0700

    Done refactor lib

---
+++ rocket_code/Module/Inc/init.h
@@ -0,0 +1,23 @@
+/*
+ * init.h
+ *
+ *  Created on: May 5, 2025
+ *      Author: Dat Hoang
+ */
+#ifndef __INIT_H__
+#define __INIT_H__
+
+#include "stm32f4xx_hal.h"
+#include APP_CONFIG
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void stm32_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ rocket_code/Module/Inc/uart.h
@@ -0,0 +1,20 @@
+/*
+ * uart.h
+ *
+ *  Created on: April 28, 2025
+ *      Author: Dat Hoang
+ */
+#ifndef __UART_H__
+#define __UART_H__
+
+#include "stdio.h"
+#include "stdint.h"
+#include "stm32f4xx_hal.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+void uart1_transmit(uint8_t *pData, uint16_t Size, uint32_t Timeout);
+#ifdef __cplusplus
+}
+#endif
+#endif

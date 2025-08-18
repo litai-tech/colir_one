d659db848907f9747dd23974db408b11e595fbe1
Dat Hoang <csdathoang88@gmail.com>
2025-08-19 04:15:30 +0700

    move folder out

--- rocket_code/Examples/sdcard/colir_one_log/build/log.d
+++
@@ -1,6 +0,0 @@
-build/log.o: /home/dat/Documents/colir_one/rocket_code/Module/Src/log.c \
- /home/dat/Documents/colir_one/rocket_code/Module/Inc/log.h Inc/config.h \
- /home/dat/Documents/colir_one/rocket_code/Module/Inc/util.h
-/home/dat/Documents/colir_one/rocket_code/Module/Inc/log.h:
-Inc/config.h:
-/home/dat/Documents/colir_one/rocket_code/Module/Inc/util.h:
